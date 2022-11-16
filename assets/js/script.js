e0a12485196fcf14ea6181f56e92e2616503c085
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-11-16 05:51:43 -0800

    added css properties for basic layout, weather icon, date span elements

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// display current date, time in current forecast area
+var currentDate = document.querySelector("#current-date")
+currentDate.textContent = currentTime.format("dddd, MMMM Do, YYYY")
+
+
