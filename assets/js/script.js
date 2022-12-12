96df44cd4e008efb5e8bf13b8043fbb060644120
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-12-12 07:52:06 -0800

    conwole logging res for 5-day forecast divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var apiKey = '7fc857720d5a4ee4808c383429e02f26'
+var data = JSON.parse(localStorage.getItem('currentSearch')) || [];
+
 
 // display current date in current current weather area html
 const d = new Date()
@@ -10,21 +12,14 @@ document.getElementById('month').innerHTML = month
 document.getElementById('year').innerHTML = year
 
 // display forecast dates in card headers: HOW??
-function addOneDay(date) {
-  date.setDatae(date.getDate() + 1)
-  return date
-}
-
-let day1 = date.addOneDay()
-let month1 = date.getMonth()
 
 // capture search input, display current search, save to localStorage as array
 $('.btn').on("click", function (event) {
   //display current search city name
   event.preventDefault()
   var currentSearch = $('input[name=city]').val()
-  localStorage.setItem('currentSearch', currentSearch)
-  var data = JSON.parse(localStorage.getItem('data')) || []
+  //localStorage.setItem('currentSearch', currentSearch)
+  var data = JSON.parse(localStorage.getItem('currentSearch')) || []
   data.push(currentSearch)
   localStorage.setItem('currentSearch', JSON.stringify(data))
 
@@ -32,27 +27,29 @@ $('.btn').on("click", function (event) {
   listSearches(data)
 
   coord(currentSearch)
-
-  //display current search city name in main dashboard
-  $('#current-city').text(currentSearch)
 })
 
-//display saved searches as list up to 8
+//display saved searches as list
 const listSearches = function (data) {
-
+  $('#list-searches').text("");
   for (var i = 0; i < data.length; i++) {
-    var li = '<li id=list-previous></li>'
+    //var li = '<li id=list-previous></li>'
+    var li = $("<li>");
+    li.attr("data-city",data[i])
+    //$('list-previous').addClass('searches')
+    li.text(data[i])
     $('#list-searches').append(li)
-    $('li').text([data[i].currentSearch])
-    $('#list-previous').addClass('btn btn-secondary')
-    //for loop to give id to each btn up to 8 searches?
+    li.addClass('btn btn-secondary li-city')
+    //give id to li
   }
 }
+//onclick pass value of btn to coord function
+//$('.btn-secondary').on('click', coord(this.text))
 
-listSearches()
-
-//add onclick to display previous search data again by pass value of btn to coord function
-$()
+$(document).on("click", ".li-city", function(){
+  var cityName = $(this).attr("data-city");
+  coord(cityName);
+})
 
 //use api to find lat and lon of city, send to currentWeatherData function and forecastWeatherData functions
 function coord(city) {
@@ -90,17 +87,17 @@ function currentWeatherData(lat, lon) {
       $('#currentHum').text(currentHum)
       $('#current-icon').attr('src', `http://openweathermap.org/img/wn/${currentIcon}@2x.png`)
     })
-  console.log(JSON.stringify(currentWeatherData))
+  //console.log(JSON.stringify(currentWeatherData))
 }
 
 //use api to get forecast weather data for current search's lat and and lon
 function forecastWeatherData(lat, lon) {
-  fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+  fetch(`https://api.openweathermap.org//data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
     .then(function (res) {
       return res.json()
     })
   //save forecast weather data as vars ... no no no no no they'll all be the same 
-  let forecastTemp1 = res.list.main.temp
+  /*let forecastTemp1 = res.list.main.temp
   let forecastWind1 = res.list.main.wind.speed
   let forecastHum1 = res.main.humidity
   let forecastTemp2 = res.list.main.temp
@@ -114,14 +111,35 @@ function forecastWeatherData(lat, lon) {
   let forecastHum4 = res.main.humidity
   let forecastTemp5 = res.list.main.temp
   let forecastWind5 = res.list.main.wind.speed
-  let forecastHum5 = res.main.humidity
+  let forecastHum5 = res.main.humidity*/
     //display data from api in forecast div in html ... I know this isn't the right way...
     .then(function (res) {
-      $('#forecastTemp1').text(forecastTemp1)
+      console.log(res);
+     /* $('#forecastTemp1').text(forecastTemp1)
       $('#forecastWind1').text(forecastWind1)
-      $('#forecastHum1').text(forecastHum1)
+      $('#forecastHum1').text(forecastHum1)*/
+      for(var i=0;i<res.length;i+8)
+      {
+        console.log(i);
+        /*var div = $("<div>");
+        div.addClass("col-2 card forecast-card");
+
+        var dt = $("<p>");
+        //dt.text(res.list[i].dt_text.split(" "));
+
+        div.append(dt);
+
+        $("#forecast-div").append(div); */
+      }
+
+      
+
+    
+
+      
     })
-  console.log(JSON.stringify(data))
+  //console.log(JSON.stringify(data))
 }
 
+listSearches(data);
 //how to re-submit saved city name to search field again on click? 
